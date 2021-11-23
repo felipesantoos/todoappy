38d5e4f464f47446fdb23f42cdaa6fcad8521329
felipesantosds <felipedssantos.dev@gmail.com>
2021-11-23 13:01:17 -0300

    Add remove todo feature

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ import db
 import msg
 
 def main():
+    INITIAL_MENU = 0
     NEW_TODO = 1
     COMPLETE_TODO = 2
+    REMOVE_TODO = 3
 
     while True:
         msg.show_header()
         msg.show_todos()
 
         try:
-            option = int(input("Escolha uma das opções:\n1 - Nova tarefa\n2 - Concluir tarefa\nOpção: "))
+            print("Escolha uma das opções:")
+            print("1 - Adicionar tarefa\n2 - Concluir tarefa\n3 - Excluir tarefa")
+            option = int(input("Opção: "))
 
             if option == NEW_TODO:
                 msg.show_option_new_todo()
             elif option == COMPLETE_TODO:
                 msg.show_option_complete_todo()
+            elif option == REMOVE_TODO:
+                msg.show_option_remove_todo()
+            elif option == INITIAL_MENU:
+                msg.show_header()
             else:
                 print("Opção inválida, por favor, digite o número 1 ou o 2.")
         except ValueError as error:
--- msg.py
+++ msg.py
@@ -7,24 +7,29 @@ def show_header():
     print("-" * COL_NUM)
     print("{:^60}".format("TASKS"))
     print("-" * COL_NUM)
-    print("{:^60}".format("Digite 99 para voltar ao menu inicial ou Ctrl + C para sair."))
+    print("{:^60}".format("Digite 0 para voltar ao menu inicial ou Ctrl + C para sair."))
     # print("-" * COL_NUM)
 
 def show_todos():
     for todo in db.get_todos():
-        check = u'\u2713' if todo[2] == 1 else ""
-        print("- [{:>4}] {:<47} {:^3}".format(todo[0], todo[1], check))
+        check = u'\u2713' if todo[2] == 1 else " "
+        id = "[#" + str(todo[0]) + "]"
+        print("- {:<5} {:<46} [{:^1}]".format(id, todo[1], check))
     print("-" * 60)
 
 def show_option_new_todo():
     task = input("Digite a descrição da tarefa: ")
     if (task != INITIAL_MENU):
-        print("Adicionando a tarefa \"{}\"...".format(task))
         db.add(task)
 
 def show_option_complete_todo():
     id = int(input("Digite o código da tarefa que quer concluir: "))
     if (id != INITIAL_MENU):
-        print("Concluíndo a tarefa #{}...".format(id))
         db.complete(id)
+
+def show_option_remove_todo():
+    id = int(input("Digite o código da tarefa que deseja excluir: "))
+    if (id != INITIAL_MENU):
+        db.remove(id)
+
 
